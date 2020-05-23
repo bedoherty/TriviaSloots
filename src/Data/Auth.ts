b86c8ef40134aaa88f1d63c99b11da964161122e
Brian Doherty <bdoherty@clintonelectronics.com>
2020-05-23 00:45:20 -0500

    Started implementing Redux/Auth libraries

---
+++ src/Data/Auth.ts
@@ -0,0 +1,7 @@
+import createAuth0Client from '@auth0/auth0-spa-js';
+
+export const auth0Client = createAuth0Client({
+    domain: "bedoherty.auth0.com",
+    client_id: "yXxvDsbQ1cjWJbbLCi657xVed1gFSgr7",
+    redirect_uri: "http://localhost:3000/auth"
+});
