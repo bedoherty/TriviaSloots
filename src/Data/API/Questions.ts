f86512d275516e9db091dc498de0f4ecd8092bc1
Brian Doherty <bdoherty@clintonelectronics.com>
2020-06-01 02:08:22 -0500

    Support no search string

--- src/Data/API/Questions.ts
+++ src/Data/API/Questions.ts
@@ -4,11 +4,17 @@ import { IDataPage } from "../Interfaces/Pagination";
 import { IQuestion } from "../Interfaces/Questions";
 
 export const getQuestionsPage = (perPage: number, page: number, search?: string) => {
-    return GET("questions/list", {
+    const options = {
         perPage,
         page,
         search
-    });
+    };
+
+    if (search?.length === 0) {
+        delete options.search;
+    }
+
+    return GET("questions/list", options);
 }
 
 export const createQuestion = (question: IQuestion) => {
